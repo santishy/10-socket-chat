93db94b965212132009935821820e86b2e29d0e5
santishy <santi_shy@hotmail.com>
2021-11-25 19:21:17 -0600

    comprobando token en los websockets

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,6 +1,10 @@
+const { checkToken } = require("../helpers");
 
-const socketController = (socket) => {
-    console.log('conectado')
+const socketController = async(socket) => {
+    const user = await checkToken(socket.handshake.headers['x-token']);
+    if(!user)
+       return socket.disconnect();
+    console.log('conectado ',user.name)
 }
 
 module.exports = {socketController}
